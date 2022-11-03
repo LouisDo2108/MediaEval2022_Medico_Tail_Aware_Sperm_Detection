5d2563c81c714f4d7f8c0d448147c5bbffaa8ce4
htluc <htluc@selab1-enp3s0>
2022-11-03 06:13:55 +0000

    Update readme.md

--- run.sh
+++ run.sh
@@ -14,10 +14,10 @@ eval "$(conda shell.bash hook)"
 conda activate medico
 
 # Convert video to images
-# CUDA_LAUNCH_BLOCKING=1 
-# python /home/htluc/mediaeval2022_medico/submission_code/code/video2img.py \
-# --source /home/htluc/datasets/VISEM_Tracking_Train_v4/Val/11/11.mp4 \
-# --weights /home/htluc/mediaeval2022_medico/submission_code/models/yolov7.pt
+CUDA_LAUNCH_BLOCKING=1 
+python /home/htluc/mediaeval2022_medico/submission_code/code/video2img.py \
+--source /home/htluc/datasets/VISEM_Tracking_Train_v4/Val/11/11.mp4 \
+--weights /home/htluc/mediaeval2022_medico/submission_code/models/yolov7.pt
 
 # Yolov7 detection
 CUDA_LAUNCH_BLOCKING=1 
