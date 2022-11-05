9c3a9c946274e1487897ba1c28723200d2c59cca
htluc <htluc@selab1-enp3s0>
2022-11-05 06:54:18 +0000

    Finalize

--- source_code/code_and_checkpoints/run_detect.sh
+++ source_code/code_and_checkpoints/run_detect.sh
@@ -4,10 +4,10 @@ conda activate medico
 
 # Yolov7 detection
 CUDA_LAUNCH_BLOCKING=1  
-python ./code/yolov7/detect.py \
+python ./yolov7/detect.py \
 --img-size 640 \
 --source ./videos/66.mp4 \
 --weights ./models/yolov7.pt \
---project ./predictions \
+--project ../../predictions/ \
 --exist-ok --save-txt \
 # --suppress
--- source_code/code_and_checkpoints/run_track.sh
+++ source_code/code_and_checkpoints/run_track.sh
@@ -4,10 +4,10 @@ conda activate medico
 
 # SORT tracking
 CUDA_LAUNCH_BLOCKING=1  
-python ./code/track.py \
+python ./track.py \
 --img-size 640 \
 --source ./videos/66.mp4 \
 --yolo-weights ./models/yolov7.pt \
---project ./predictions/ \
+--project ../../predictions/ \
 --exist-ok --save-txt --save-vid 
 # --suppress \
