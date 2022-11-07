14561967133cbb0d0b304e2e2510329c7f066985
htluc <htluc@selab1-enp3s0>
2022-11-07 08:14:38 +0000

    fix bugs

--- source_code/code_and_checkpoints/SORT_yolov5/tools/demo_track.py
+++ source_code/code_and_checkpoints/SORT_yolov5/tools/demo_track.py
@@ -510,6 +510,9 @@ def run(
 
             prev_frames[i] = curr_frames[i]
 
+    for vw in vid_writer:
+        if isinstance(vw, cv2.VideoWriter):
+            vw.release()
     # Print results
     # t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
     # LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS, %.1fms strong sort update per image at shape {(1, 3, *img_size)}' % t)
